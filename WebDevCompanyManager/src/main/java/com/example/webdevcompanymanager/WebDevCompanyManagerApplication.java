85e7b1decc3dff545620bd8a26a0c270b3bf5ede
unknown <ckw924@cpt.local>
2026-02-12 14:30:28 +0100

    12.02.26 First Commit

---
+++ WebDevCompanyManager/src/main/java/com/example/webdevcompanymanager/WebDevCompanyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdevcompanymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevCompanyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevCompanyManagerApplication.class, args);
+    }
+
+}
---
+++ WebDevCompanyManager/src/test/java/com/example/webdevcompanymanager/WebDevCompanyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevcompanymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevCompanyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
